bbb707976d1f305044b0b5a9b6e3f45e4d8d15d9
Rohan Taneja <rohantaneja63@gmail.com>
2016-05-29 19:29:02 +0530

    added proprietary files

---
+++ j805-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/j805/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/j805/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/j805/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/j805/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/j805/proprietary/vendor,system/vendor)
